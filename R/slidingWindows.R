c17dab6caa7f4d2d396cdf9011581f8b48052571
Valeriy Avanesov <avanesov@wias-berlin.de>
2016-11-01 17:10:43 +0100

    Licence fix

--- R/slidingWindows.R
+++ R/slidingWindows.R
@@ -1,15 +1,22 @@
+# R package for change-point detection in covariance structure
+# Copyright (C) 2016 Valeriy Avanesov acopich@gmail.com 
+
 slidingWindows = function(data, windowSize, getParameters, parameterDistance) {
   parameters = lapply(1:(nrow(data) - windowSize + 1), function(i) {
     windowData = data[i:(i + windowSize - 1), ]
     getParameters(windowData)
   })
   
-  distances = sapply(1:(length(parameters) - windowSize), function(i) {
+  numberOfEstimators = length(parameters)
+  
+  distances = sapply(1:(numberOfEstimators - windowSize), function(i) {
     left = parameters[[i]]
     right = parameters[[i + windowSize]]
     
     parameterDistance(left, right)
   })
   
-  distances
+  centralPoints = (windowSize + 1):numberOfEstimators
+  
+  list(centralPoints, distances)
 }
