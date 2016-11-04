665e6ff2566fa1cc8028526fbcfc30e645598460
Valeriy Avanesov <avanesov@wias-berlin.de>
2016-11-04 16:41:40 +0100

    plot fix

--- R/plotStats.R
+++ R/plotStats.R
@@ -3,7 +3,12 @@ plotStats = function(statsAndCritValue) {
   crit = statsAndCritValue$criticalValue
   
   for (forWindow in stats) {
-    plot(forWindow$centralPoints, forWindow$distances, col = ifelse(forWindow$distances > crit,"red", "black"), ylab = "Parameter distance", xlab = "Central point")
+    plot(forWindow$centralPoints, 
+         forWindow$distances, 
+         col = ifelse(forWindow$distances > crit,"red", "black"), 
+         xlab = "Central point",
+         ylab = "Parameter distance", 
+         ylim = c(0, max(crit, max(forWindow$distances))))
     title(paste("Window size = ", forWindow$windowSize))
     abline(h = crit, col = "red")
   }
