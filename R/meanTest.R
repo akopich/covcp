225c5b8db32b2cf695b2b4af1e5f2ac0b10cdb80
Valeriy Avanesov <avanesov@wias-berlin.de>
2016-11-04 16:38:06 +0100

    mean test

---
+++ R/meanTest.R
@@ -0,0 +1,62 @@
+getMeanEstimator = function(window) function(data) {
+  colSums(data) / sqrt(window)
+}
+
+createMeanTest = function(windowSizes, 
+                                     alpha,
+                                     data, 
+                                     distances2statistic, 
+                                     diffNorm, 
+                                     stableSetIndexs, 
+                                     bootstrapIterations = 1000) {
+  stableSet = data[stableSetIndexs, ]
+  
+  stats = precisionMatrixStatistic(windowSizes, 
+                                   data, 
+                                   distances2statistic, 
+                                   getMeanEstimator,
+                                   diffNorm) 
+  
+  criticalValue = meanBootstrapBasedCriticalLevel(stableSet, 
+                                          bootstrapIterations, 
+                                          alpha,
+                                          nrow(data), 
+                                          windowSizes, 
+                                          diffNorm, 
+                                          distances2statistic)
+  list("statistics" = stats, "criticalValue" = criticalValue)
+}
+
+
+
+meanBootstrapBasedCriticalLevel = function(stable, 
+                                            iterations, 
+                                            alpha,
+                                            N, 
+                                            windowSizes, 
+                                            parameterDifferenceNorm, 
+                                            distances2statistic) {
+  means = colMeans(stable)
+  stable = sweep(stable, 2, means, '-')
+  
+  bootstrappedValues = parSapply(1:iterations, function(iter) {
+    bootstrapSample = drawWithReplacement(stable, N)
+    
+    maxSapply(windowSizes, function(windowSize) {
+      distances2statistic(slidingWindowsDifferenceOfMean(bootstrapSample * sqrt(windowSize), 
+                                                         windowSize, 
+                                                         parameterDifferenceNorm))
+    })
+  })
+  
+  unname(quantile(bootstrappedValues, probs = c(1 - alpha)))
+}
+
+
+
+
+
+
+
+
+
